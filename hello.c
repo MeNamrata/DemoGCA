6be5821f3c212b7c791781f91f1e5d35924bac6f
Namrata Bhandari <bhandari.namta@gmail.com>
2020-10-18 15:28:02 +0545

    changed hello.txt to hello.c

---
+++ hello.c
@@ -0,0 +1,11 @@
+// hello hello GCA online training here
+// 3 days workshop Hello its me Namrata Bhandari
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	int n=3;
+	printf("hello hello GCA online training here
+%d days workshop Hello its me Namrata Bhandari
+\n",n);
+	return 0;
+}
